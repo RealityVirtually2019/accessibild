78181d2caf038f903268f89307c1556b05425578
cpapenfuss <chris.papenfuss@data-experts.de>
2019-01-18 15:21:10 -0500

    Basic layout working

---
+++ HoloLens/Assets/Scripts/AppStateManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class AppStateManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+#if !UNITY_EDITOR
+        UnityEngine.SceneManagement.SceneManager.LoadScene("BathRoomScanning", UnityEngine.SceneManagement.LoadSceneMode.Additive);
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ HoloLens/Assets/Scripts/HintBox.cs
@@ -0,0 +1,25 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintBox : Singleton<HintBox> {
+
+    private Text _textObject;
+
+	// Use this for initialization
+	void Start () {
+        _textObject = GetComponentInChildren<Text>();
+	}
+
+    public void ShowText(string text)
+    {
+        _textObject.text = text;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
